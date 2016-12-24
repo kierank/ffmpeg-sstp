9ce10f5cbb8b5a3fb0ceea5fa1c609b7faa3eb41
Kieran Kunhya <kieran@kunhya.com>
2016-12-24 17:23:07 +0000

    Check bits left when looking for next startcode

--- libavcodec/mpeg4videodec.c
+++ libavcodec/mpeg4videodec.c
@@ -2660,7 +2660,7 @@ static void next_start_code_studio(GetBitContext *gb)
 {
     align_get_bits(gb);
 
-    while (show_bits_long(gb, 24) != 0x1) {
+    while (get_bits_left(gb) >= 24 && show_bits_long(gb, 24) != 0x1) {
         get_bits(gb, 8);
         printf("reading byte \n");
     }
